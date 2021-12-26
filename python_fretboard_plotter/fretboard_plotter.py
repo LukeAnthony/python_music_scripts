4811997e30234323ac2b62da3a359814b3e534e2
Luke <luke.bonomo1@gmail.com>
2021-12-25 23:19:12 -0500

    wip combining sharps and flats into strings

--- python_fretboard_plotter/fretboard_plotter.py
+++ python_fretboard_plotter/fretboard_plotter.py
@@ -7,8 +7,10 @@ import matplotlib.patches as mpatches
 # 	and https://betterprogramming.pub/how-to-learn-guitar-with-python-978a1896a47. 
 #	Thank you to Diego Penilla for making this easy for me
 class FretboardPlotter:
-	# a sufficiently long list to take 12 notes out of any note
-	whole_notes = ['C' , 'C#', 'D', 'D#', 'E', 'F', 'F#' , 'G', 'G#', 'A', 'A#', 'B']*3
+	# multiplying lists by 3 bc of populate strings method. looks 21 notes past the starting point of the strings, 
+	# 	so need to make sure there's enough notes to choose from
+	whole_notes_flats = ['C' , 'Db', 'D', 'Eb', 'E', 'F', 'Gb' , 'G', 'Ab', 'A', 'Bb', 'B']*3
+	whole_notes_sharps = ['C' , 'C#', 'D', 'D#', 'E', 'F', 'F#' , 'G', 'G#', 'A', 'A#', 'B']*3
 	strings = {}
 
 	# Enums for fretboard
@@ -23,19 +25,23 @@ class FretboardPlotter:
 		self.populate_strings()
 
 	def get_notes(self, key, intervals):
-		"""Diego: "Given any key C, C#... B
-    	and intervals z.B Tone Tone Semitone" """
-		root = self.whole_notes.index(key)
-		octave = self.whole_notes[root:root+12]
+		containsSharps = '#' in key
+		root = self.whole_notes_sharps.index(key) if containsSharps else self.whole_notes_flats.index(key)
+		octave = self.whole_notes_sharps[root:root+12] if containsSharps else self.whole_notes_flats[root:root+12]
 		return [octave[i] for i in intervals]
 
+	# need to populate strings as a map of {String -> List[Tuples()]}
 	def populate_strings(self):
-		start_strings = []
 		strings = {i:0 for i in 'EADG'}
 		for i in strings.keys():
-			start = self.whole_notes.index(i)
-			strings[i] = self.whole_notes[start:start+21]
+			# combines sharps and flats into a list of tuples, with equal accidentals occupying the same index
+			# 	[('C', 'C'), ('C#', 'Db'), ('D', 'D'), ('D#', 'Eb'), ('E', 'E'), ('F', 'F'), ('F#', 'Gb'), ('G', 'G'), ('G#', 'Ab').....
+			sharpsAndFlats = list(zip(self.whole_notes_sharps, self.whole_notes_flats))
+			#start = index of the first tuple containing E|A|D|G in sharpsAndFlats
+			start = [y[0] for y in sharpsAndFlats].index(i)
+			strings[i] = sharpsAndFlats[start:start+21]
 		self.strings = strings
+		print(strings)
 
 	def find_notes(self, scale):
 	    notes_strings = {i:0 for i in "EADG"}
@@ -54,46 +60,36 @@ class FretboardPlotter:
 	        notes_strings[key] = indexes
 	    return notes_strings
 
-	# TODO iterate through ax array rather than referencing each time. will be cleaner
+	#index 0 = right handed plot, index 1 = left handed plot
 	def plot(self, key, intervals, graphTitle, night=True):
 		scale = self.get_notes(key, intervals)
-		# Diego: "Plot Strings"
+		#creating two plots, top right handed, bottom left handed
 		fig, ax = plt.subplots(2, figsize=(21,4))
 		background = ['white', 'black']
 		# creates 4 lines in each plot for the 4 strings
 		for i in range(1,5):
-			for graph in ax:
+			for index, graph in enumerate(ax):
 				graph.plot([i for a in range(22)])
-			#ax[0].plot([i for a in range(22)])
-			#ax[1].plot([i for a in range(22)])
+				graph.set_axisbelow(True)
+				#setting height and width of displayed guitar
+				if index == 0:
+					graph.set_xlim([0.45, 21])
+				elif index == 1:
+					graph.set_xlim([0, 20.1])
+				graph.set_ylim([0.4, 5])
+				graph.set_facecolor(background[night])
 		
 		#creates the lines to simulate the fretboard in each graph
 		for i in range(1,21):
-			# lefty --> i == 9? 8?
-			# if i == 9:
-			# righty
-			if i == 9:
-				ax[0].axvline(x=i, color='gray', linewidth=3.5)
-				ax[1].axvline(x=i, color='gray', linewidth=3.5)
-				continue
-			if i == 12:
-				ax[0].axvline(x=i, color='gray', linewidth=3.5)
-				ax[1].axvline(x=i, color=background[night-1], linewidth=0.5)
-				continue
-			ax[0].axvline(x=i, color=background[night-1], linewidth=0.5)
-			ax[1].axvline(x=i, color=background[night-1], linewidth=0.5)
-
-		# Diego: "ax.grid(linestyle='-', linewidth='0.5', color='black')"
-		ax[0].set_axisbelow(True)
-		ax[1].set_axisbelow(True)
-		
-		# Diego: "setting height and width of displayed guitar"
-		ax[0].set_xlim([0.5, 21])
-		ax[1].set_xlim([0.5, 21])
-		ax[0].set_ylim([0.4, 5])
-		ax[1].set_ylim([0.4, 5])
-		ax[0].set_facecolor(background[night])
-		ax[1].set_facecolor(background[night])
+			for index, graph in enumerate(ax):
+				# draw gray line completely around 12 fret
+				if (index == 0 and (i == 12 or i ==13)) or (index == 1 and (i == 8 or i == 9)):
+					graph.axvline(x=i, color='gray', linewidth=4.5)
+				# draw gold line before first fret to symbolize open notes
+				elif (i == 1 and index == 0) or (i == 20 and index == 1):
+					graph.axvline(x=i if index == 1 else i-.2, color='gold', linewidth=6.5)
+				else:
+					graph.axvline(x=i, color=background[night-1], linewidth=0.5)
 		to_plot = self.find_notes(scale)
 		
 		#find_notes returns a map of strings with the indexes of the notes that are in the given scale
@@ -105,73 +101,34 @@ class FretboardPlotter:
 				font = 12
 				color = 'w'
 				circleSize = 0.2
-				#lefty
-				xLefty = 21 - i - 0.3
-				#righty 
-				xRighty = i + 0.55
+				# i = 0 means an open string note (E,A,D,G)
+				xLefty = (21 - i - 0.7) if i == 0 else (21 - i - 0.45)
+				xRighty = (i + 0.55) if i == 0 else (i + 0.55)
 				# strings is a map of each string and their notes. 
 				# gets the list of notes for a given string and looks up the note at the index
 				note = self.strings[key][i]
-				print("Note = " + note)
 				# make the root slightly larger and yellow
 				if note == scale[0]:
 					font = 15
 					color = 'yellow'
 					circleSize = 0.3
-				p = mpatches.Circle((xRighty, y_val), circleSize)
-				pLefty = mpatches.Circle((xLefty, y_val), circleSize)
-				ax[0].add_patch(p)
-				ax[1].add_patch(pLefty)
-				ax[0].annotate(note, (xRighty, y_val), color=color, weight='bold', fontsize=font, ha='center', va='center')
-				ax[1].annotate(note, (xLefty, y_val), color=color, weight='bold', fontsize=font, ha='center', va='center')
+				for index, graph in enumerate(ax):
+					p = mpatches.Circle((xRighty if index == 0 else xLefty, y_val), circleSize)
+					graph.add_patch(p)
+					graph.annotate(note, (xRighty if index == 0 else xLefty, y_val), color=color, weight='bold', fontsize=font, ha='center', va='center')
 
-		#plt.title(graphTitle)
 		fig.suptitle(graphTitle + "\n Righty (top graph). Lefty (bottom graph)")
+		# adding more space between the two graphs
 		plt.subplots_adjust(None, None, None, None, None, 0.5)
-		ax[1].yaxis.tick_right()
-
-		ax[0].set_yticks(np.arange(1,5), ['E', 'A', 'D', 'G'])
-		ax[1].set_yticks(np.arange(1,5), ['E', 'A', 'D', 'G'])
-		#plt.yticks(np.arange(1,5), ['E', 'A', 'D', 'G'])
-
-		# Diego had offset at +0.5. shifting down slightly since i think it's easier to see open strings this way
-		# plt.xticks(np.arange(21)+0.45, np.arange(0,20))
-		ax[0].set_xticks(np.arange(21)+0.45, np.arange(0,21))
-		ax[1].set_xticks(np.arange(21)+0.45, np.arange(-20,1))
-		#plt.xticks(np.arange(21)+0.45, np.arange(-20,1))
+		for index, graph in enumerate(ax):
+			if index == 0:
+				graph.set_xticks(np.arange(21)+0.45, np.arange(0,21))
+			elif index == 1:
+				graph.yaxis.tick_right()
+				graph.set_xticks(np.arange(21)+0.45, np.arange(20,-1,-1))
+			graph.set_yticks(np.arange(1,5), ['E', 'A', 'D', 'G'])
 		plt.show()
 
 fretboardPlotter = FretboardPlotter()
 
-# index where element 'B' is located.
-root = fretboardPlotter.whole_notes.index('B')
-# starting from this index, slice twelve elements
-B = fretboardPlotter.whole_notes[root:root+12]
-print('B chromatic scale: ', B)
-
-
-# intervals of the major scale as indexes:
-major_scale = [0, 2, 4, 5, 7, 9, 11]
-# selecting the indexes of the octave we want
-notes = [B[i] for i in major_scale]
-print('B major scale:', notes)
-
-
-
-another_scale = [0, 2, 5, 10, 11]
-notes = [B[i] for i in another_scale]
-print('Some scale ', notes)
-
-
-print('Notes of C major scale:\n{}'.format(fretboardPlotter.get_notes('C', FretboardPlotter.MINOR_SCALE)))
-
-
-cMajorNotes = fretboardPlotter.get_notes("C", fretboardPlotter.MAJOR_SCALE)
-print()
-print(cMajorNotes)
-
-cMajorNotesOnStrings = fretboardPlotter.find_notes(cMajorNotes)
-print(cMajorNotesOnStrings)
-
-print(fretboardPlotter.strings)
-fretboardPlotter.plot('B', fretboardPlotter.MAJOR_SCALE, "C Major Scale")
+fretboardPlotter.plot('Bb', fretboardPlotter.MAJOR_SCALE, "Bb Major Scale")
