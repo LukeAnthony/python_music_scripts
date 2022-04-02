2ca561c19a7ac8a7415208454acec03f6f379fa8
Luke <LukeAnthony@users.noreply.github.com>
2022-04-02 09:01:44 -0400

    added more notes to pcn

--- python_chord_notation_quiz/python_chord_notation_quiz.py
+++ python_chord_notation_quiz/python_chord_notation_quiz.py
@@ -9,24 +9,58 @@ The user will be able to select which mode they want (either only 1, only 2, or 
 Stats will be kept along the way
 
 
-How to type symbols on mac
-	option k = ˚ (diminished)
-	option o = ø (half-diminished chords)
-	option j = ∆ (major)
-	+ (augmented)
-	# (augmented)
-	b for flat
+How to type symbols on mac, windows
+	diminished
+		option k  = ˚
+		alt 248 = ° 
+		(need to figure out how to get that on mac...they clearly are different) 
+	half-diminished chords
+		option o = ø
+		alt 0248 = ø
+	major
+		option j = ∆
+		alt 30? = ▲
+	augmented
+		+, #
+	flat
+		b
 """
 
-# Chords supported
-	# major 
-		# major triad
-		# major seventh
-	# minor 
-		# minor triad
-		# minor seventh
-	# diminished
-	# augmented
-	# suspended 
-	# dominant
-		# dominant seventh
+"""
+The formula for notating chords is:
+	chord = rqe
+	r = root of the chord 
+		ex) C, D, C#
+	q = quality of the chord
+		ex) major, minor, half-diminished, altered
+	e = extensions
+		ex) 7, add9
+
+Chords supported
+	major 
+		C, D, C# = major triad
+		CM || CM7 = major seventh
+		C6, C2, C4 = major w/ extra notes
+	minor 
+		Cm, Dm, C#m || C-, D-, C#- = minor triad
+		minor seventh
+	diminished
+		diminished triad
+	augmented
+		C+, Caug = augmented triad
+	suspended 
+	dominant
+		dominant seventh
+	half-diminished
+	altered chord
+	slash chord
+	extensions
+"""
+class PythonChordNotation:
+	isFirstAttempt = True
+	
+	@staticMethod
+	def getSettings():
+
+
+while(True):
